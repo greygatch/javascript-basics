7a2b170467dfd6af39378767e3d37cd633d730b4
Chyld Medford <chyld.medford@gmail.com>
2015-04-06 17:20:24 -0700

    math

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-alert('hey');
+var a = prompt('give me a num?');
+a = parseInt(a);
+var b = prompt('give me a another num?');
+b = parseInt(b);
+var c = Math.pow(a,b);
+console.log('a', a, 'b', b, 'c', c);
