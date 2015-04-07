ca3fc34519f4c10963c65ba400d267f996cc05a2
Evan Gatchell <greygatch@gmail.com>
2015-04-07 13:45:00 -0700

    bugz

--- main.js
+++ main.js
@@ -1,28 +1,14 @@
-<<<<<<< HEAD
 function numberPlay (str) {
 
     var num = str.split(":");
     var spex = {};
 
     spex.sum = (num.map(function(e){return parseInt(e)})).reduce(function(a, b, i, e) {return a + b});
-    spex.products = num.reduce(function(a, b, i, e) {return a * b});
+    spex.products = (num.map(function(e){return parseInt(e)})).reduce(function(a, b, i, e) {return a * b});
     spex.avg = spex.sum / num.length;
-    spex.stdDev = Math.sqrt((num.map(function(e){return (e * e) - spex.avg;}).reduce(function(a, b, i, e) {return a + b})) / 2);
+    spex.stdDev = Math.sqrt((num.map(function(e){return (e * e) - spex.avg;}).reduce(function(a, b, i, e)                {return a + b})) / 2);
 
     return spex;
 }
 
-var inp = prompt("Give me a number. (ex: n:n:n )");
-numberPlay(inp);
-=======
-var a = parseInt(prompt("gimme an int, dawg: "));
-var b = parseInt(prompt("gimme an another int: "));
-var c = prompt("do what with em? (+, -, *, /)");
-
-if (c === "+" || c === "-"){
-  c === "+" ? alert(a + b) : alert (a - c);
-}
-else if (c === "*" || c === "/") {
-  c === "*" ? alert(a * b) : alert(a / b);
-}
->>>>>>> a00e84dbc6c1f43ede0d9b8642faf24fc8910259
+console.log(numberPlay("7:8:9"));
