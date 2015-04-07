4269d35d987233e67711e050a16230abcc8ee76d
Evan Gatchell <greygatch@gmail.com>
2015-04-07 13:17:03 -0700

    spaces

--- main.js
+++ main.js
@@ -8,9 +8,7 @@ function numberPlay (str) {
     spex.avg = sum / int.length;
     spex.stdDev = Math.sqrt((int.map(function(e){return (e * e) - avg;}).reduce(function(a, b, i, e) {return a + b})) / 2);
 
-
     return spex;
-
 }
 
 var inp = prompt("Give me a number. (ex: n:n:n )");
