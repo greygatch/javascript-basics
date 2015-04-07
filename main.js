9d75e4d693d2e4d3ebc0ec8f72960f096ff868e3
Jon Edwards <jkedwards@me.com>
2015-04-06 17:30:28 -0700

    complete problem1

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
-var a = prompt('give me a num?');
+var a = prompt('Enter the first number.');
 a = parseInt(a);
-var b = prompt('give me a another num?');
-b = parseInt(b);
-var c = Math.pow(a,b);
-console.log('a', a, 'b', b, 'c', c);
+var b = prompt('Enter an arithmetic operator ( + , - , * or / ).');
+var c = prompt('Please enter the second number.');
+c = parseInt(c);
+var result;
+switch (b) {
+    case '+':
+      result = a + c;
+      break;
+    case '-':
+      result = a - c;
+      break;
+    case '*':
+      result = a * c;
+      break;
+    case '/':
+      result = a / c;
+      break;
+    default:
+      result = undefined;
+}
+console.log('The result of a & b is', result);
