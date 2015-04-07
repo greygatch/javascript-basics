4033edc6af6167609f8dd038242653ed96d4ad37
Evan Gatchell <greygatch@gmail.com>
2015-04-06 17:57:45 -0700

    code golf

--- main.js
+++ main.js
@@ -2,16 +2,9 @@ var a = parseInt(prompt("gimme an int, dawg: "));
 var b = parseInt(prompt("gimme an another int: "));
 var c = prompt("do what with em? (+, -, *, /)");
 
-if (c === "+"){
-  alert(a + b);
+if (c === "+" || c === "-"){
+  c === "+" ? alert(a + b) : alert (a - c);
 }
-else if (c === "-") {
-  alert(a - b);
-}
-
-else if (c === "*") {
-  alert(a * b);
-}
-else {
-  alert(a / b);
+else if (c === "*" || c === "/") {
+  c === "*" ? alert(a * b) : alert(a / b);
 }
