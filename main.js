b535969ef62271d6f0bec9b304547889cae8a96c
Chyld Medford <chyld.medford@gmail.com>
2015-04-07 11:22:47 -0700

    comment problem 1

--- main.js
+++ main.js
@@ -1,21 +1,24 @@
-var a = prompt('Enter the first number.');
-a = parseFloat(a);
-var o = prompt('Enter an arithmetic operator ( + , - , * or / ).');
-var c = prompt('Please enter the second number.');
-c = parseFloat(c);
-var result;
-switch (o) {
-    case '+':
-      result = a + c;
-      break;
-    case '-':
-      result = a - c;
-      break;
-    case '*':
-      result = a * c;
-      break;
-    case '/':
-      result = a / c;
-      break;
-}
-console.log('The result of a', o, 'b is', result);
+// problem 1 : feature
+// var a = prompt('Enter the first number.');
+// a = parseFloat(a);
+// var o = prompt('Enter an arithmetic operator ( + , - , * or / ).');
+// var c = prompt('Please enter the second number.');
+// c = parseFloat(c);
+// var result;
+// switch (o) {
+//     case '+':
+//       result = a + c;
+//       break;
+//     case '-':
+//       result = a - c;
+//       break;
+//     case '*':
+//       result = a * c;
+//       break;
+//     case '/':
+//       result = a / c;
+//       break;
+// }
+// console.log('The result of a', o, 'b is', result);
+
+// problem 2 : feature
