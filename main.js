7ff73d525ead1a9550198a054470b92c06c0e289
Evan Gatchell <greygatch@gmail.com>
2015-04-07 14:57:54 -0700

    minify

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ function numberPlay (str) {
     var num = str.split(":");
     var spex = {};
 
-    spex.sum = (num.map(function(e){return parseInt(e)})).reduce(function(a, b, i, e) {return a + b});
-    spex.products = (num.map(function(e){return parseInt(e)})).reduce(function(a, b, i, e) {return a * b});
+    spex.sum = (num.map(function(e){return parseInt(e)})).reduce(function(a, b) {return a + b});
+    spex.products = (num.map(function(e){return parseInt(e)})).reduce(function(a, b) {return a * b});
     spex.avg = spex.sum / num.length;
-    spex.stdDev = Math.sqrt((num.map(function(e){return (e * e) - spex.avg;}).reduce(function(a, b, i, e)                {return a + b})) / 2);
+    spex.stdDev = Math.sqrt((num.map(function(e){return (e * e) - spex.avg;}).reduce(function(a, b){return a + b})) / 2);
 
     return spex;
 }
